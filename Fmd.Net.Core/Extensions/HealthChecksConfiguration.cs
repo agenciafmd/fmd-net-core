df1f509844fda0d0f45f98ccfc658d83a809f9dd
Vinicius Fumagalli <vinicius.fumagalli@terceiros.usebens.com.br>
2025-06-25 15:11:56 -0300

    feat(HealthChecksConfig): unifica implementação da UI no Core

---
+++ Fmd.Net.Core/Extensions/HealthChecksConfiguration.cs
@@ -0,0 +1,24 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Fmd.Net.Core.Extensions;
+
+public static class HealthChecksConfiguration
+{
+    public static void UseHealthCheckConfiguration(this IApplicationBuilder app, string? path = null, 
+        string? uiPath = null)
+    {
+        app.UseHealthChecks(path ?? "/health", new HealthCheckOptions()
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+
+        app.UseHealthChecksUI(options =>
+        {
+            options.UIPath = uiPath ?? "/health-dashboard";
+            options.AddCustomStylesheet("Styles/healthchecks.css");
+        });
+    }
+}
