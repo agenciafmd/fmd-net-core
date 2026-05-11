aebd3d20f29b80a32157f27eb52d4a24bd6d91d8
Vinicius Fumagalli <vinicius.fumagalli@terceiros.usebens.com.br>
2026-05-11 16:04:25 -0300

    feat(Fmd.Net.Core): melhora filtro para entidades genéricas

--- Fmd.Net.Core/Filters/DeletedAtFilter.cs
+++ Fmd.Net.Core/Filters/DeletedAtFilter.cs
@@ -1,6 +1,7 @@
 using System.Linq.Expressions;
 using Fmd.Net.Core.DomainObjects;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Fmd.Net.Core.Filters;
 
@@ -8,14 +9,34 @@ public static class DeletedAtFilter
 {
     public static void IgnoreDeletedAtFilter(this ModelBuilder builder)
     {
-        foreach (var entityType in builder.Model.GetEntityTypes())
+        foreach (IMutableEntityType entityType in builder.Model.GetEntityTypes())
         {
-            if (!typeof(Entity<>).IsAssignableFrom(entityType.ClrType)) continue;
-
-            var parameterName = Expression.Parameter(entityType.ClrType, "entity");
-            var property = Expression.Property(parameterName, nameof(Entity<>.DeletedAt));
-            var comparison = Expression.Equal(property, Expression.Constant(null, typeof(DateTime?)));    
-            builder.Entity(entityType.ClrType).HasQueryFilter(Expression.Lambda(comparison, parameterName));
+            if (ImplementsGenericBase(entityType.ClrType, typeof(Entity<>)))
+            {
+                var parameterExpression = Expression.Parameter(entityType.ClrType, "entity");
+                var body = Expression.Equal(
+                    Expression.Property(parameterExpression, nameof(Entity<>.DeletedAt)),
+                    Expression.Constant(null, typeof(DateTime?))
+                );
+                
+                builder
+                    .Entity(entityType.ClrType)
+                    .HasQueryFilter(Expression.Lambda(body, parameterExpression));
+            }
         }
     }
+    
+    private static bool ImplementsGenericBase(Type type, Type genericBase)
+    {
+        while (type != null && type != typeof(object))
+        {
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == genericBase)
+                return true;
+    
+            type = type.BaseType;
+        }
+        
+        return false;
+    }
+
 }
